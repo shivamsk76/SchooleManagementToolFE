c7ec296b0f5d71d621f4c31e9271604c3a32d872
unknown <st7692833267@gamil.com>
2020-09-25 15:14:07 +0530

    added homepage component

---
+++ src/components/homepage/homepage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class homepage extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello world</h1>
+            </div>
+        )
+    }
+}
+
+export default homepage
